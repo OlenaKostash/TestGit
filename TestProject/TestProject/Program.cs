0172cf5abd201af3aac957b90ff0f90e3ceca8bf
Olena Kostash <116300846+OlenaKostash@users.noreply.github.com>
2022-10-21 22:36:20 +0300

    Create new project

---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
