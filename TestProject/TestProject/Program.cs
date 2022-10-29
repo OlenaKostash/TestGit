b84f9cf6779e2339b67f8036f596181d12c8c73f
Olena Kostash <116300846+OlenaKostash@users.noreply.github.com>
2022-10-29 11:09:46 +0300

    Homework №4 - conditional operators

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -14,32 +14,29 @@ namespace TestProject
             bool isParseblY = Int32.TryParse(Console.ReadLine(), out y);
             if (isParseblX && isParseblY)
             {
-
-                var result1 = -6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15;
-                Console.WriteLine($"result from -6*x^3+5*x^2-10*x+15 = {result1}");
-                var result2 = Math.Abs(x) * Math.Sin(x);
-                Console.WriteLine($"result from abs(x)*sin(x) = {result2}");
-                var result3 = 2 * Math.PI * x;
-                Console.WriteLine($"result from 2*pi*x = {result3}");
-                var result4 = Math.Max(x, y);
-                Console.WriteLine($"result from max(x, y) = {result4}");
+                int sum = 0;
+                if (x > y)
+                   sum = CalculateSum(x, y);
+                else if (y > x)
+                   sum = CalculateSum(y, x);
+                else
+                    Console.WriteLine($"The sum is {x}");
             }
             else 
             {
                 Console.WriteLine("Incorrect symbols");
             }
-
-            Console.WriteLine("");
-            
-            DateTime date1 = new DateTime(2022, 1, 1, 0, 0, 0);
-            DateTime date2 = new DateTime(2023, 1, 1, 0, 0, 0);
-            TimeSpan intervalX = date2 - DateTime.Now; 
-            TimeSpan intervalY = DateTime.Now - date1;
-            Console.WriteLine($"{intervalX.Days} days left to New Year");
-            Console.WriteLine($"{intervalY.Days} days passed from New Year");
             Console.ReadKey();
-
+ 
         }
 
+        static int CalculateSum(int max, int min)
+        {
+            int result = 0;
+            for (int i = min; i <= max; i++)
+                result = result + i;
+            Console.WriteLine($"The sum of all numbers between {min} and {max} is {result}");
+            return result;
+        }
     }
 }
