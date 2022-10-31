63991d0049e1e7614567536079b40eac06edcc74
Olena Kostash <116300846+OlenaKostash@users.noreply.github.com>
2022-11-01 00:26:07 +0200

    Homework5 - Методи

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -7,39 +7,79 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
+            #region 1,2,4 Paragraph
+            Random random = new Random();
+            List<int> sorseArray = new List<int>(); 
+            for(int i = 0; i < 4; i++)
+                sorseArray.Add(random.Next(0, 20));
 
-            int x, y;
+            Console.WriteLine($"max number from {sorseArray[0]} and {sorseArray[1]}"
+                + " is " + MaxNumber(sorseArray[0], sorseArray[1]));
+            Console.WriteLine($"min number from {sorseArray[0]} and {sorseArray[1]}"
+               + " is " + MinNumber(sorseArray[0], sorseArray[1]));
+            #endregion
+
+            #region 3 Paragraph
+            int sum;
+            bool resultFromSum = TrySumIfOdd(sorseArray[0], sorseArray[1],out sum);
+            Console.WriteLine($"{resultFromSum}: the sum between " +
+                $"{sorseArray[0]} and {sorseArray[1]} is {sum}");
+            #endregion
+
             Console.WriteLine("Enter a value for x");
             bool isParseblX = Int32.TryParse(Console.ReadLine(), out x);
-            Console.WriteLine("Enter a value for y");
-            bool isParseblY = Int32.TryParse(Console.ReadLine(), out y);
-            if (isParseblX && isParseblY)
-            {
 
-                var result1 = -6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15;
-                Console.WriteLine($"result from -6*x^3+5*x^2-10*x+15 = {result1}");
-                var result2 = Math.Abs(x) * Math.Sin(x);
-                Console.WriteLine($"result from abs(x)*sin(x) = {result2}");
-                var result3 = 2 * Math.PI * x;
-                Console.WriteLine($"result from 2*pi*x = {result3}");
-                var result4 = Math.Max(x, y);
-                Console.WriteLine($"result from max(x, y) = {result4}");
-            }
-            else
-            {
-                Console.WriteLine("Incorrect symbols");
-            }
-
-            Console.WriteLine("");
-
-            DateTime date1 = new DateTime(2022, 1, 1, 0, 0, 0);
-            DateTime date2 = new DateTime(2023, 1, 1, 0, 0, 0);
-            TimeSpan intervalX = date2 - DateTime.Now;
-            TimeSpan intervalY = DateTime.Now - date1;
-            Console.WriteLine($"{intervalX.Days} days left to New Year");
-            Console.WriteLine($"{intervalY.Days} days passed from New Year");
             Console.ReadKey();
-
         }
+
+        #region 1,2,4 Paragraph
+        static int MaxNumber(int num1, int num2)
+        { 
+            return Math.Max(num1, num2);
+        }
+
+        static int MaxNumber(int num1, int num2, int num3)
+        {
+            return Math.Max(num1, Math.Max(num2, num3));
+        }
+
+        static int MaxNumber(int num1, int num2, int num3, int num4)
+        {
+            return Math.Max(num1, Math.Max(num2, Math.Max(num3, num4)));
+        }
+
+        static int MinNumber(int num1, int num2)
+        {
+            return Math.Min(num1, num2);
+        }
+
+        static int MinNumber(int num1, int num2, int num3)
+        {
+            return Math.Min(num1, Math.Min(num2, num3));
+        }
+
+        static int MinNumber(int num1, int num2, int num3, int num4)
+        {
+            return Math.Min(num1, Math.Min(num2, Math.Min(num3, num4)));
+        }
+        #endregion
+
+        #region 3 Paragraph
+        static bool TrySumIfOdd(int num1, int num2, out int sum)
+        {
+            sum = 0;
+
+            for (int i = Math.Min(num1, num2)+1; i < Math.Max(num1, num2);i++)
+                sum = sum + i;
+
+            if (sum % 2 == 0)
+                return false;
+            else
+                return true; 
+        }
+        #endregion
+
+
+
     }
 }
