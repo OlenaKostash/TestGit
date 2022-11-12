884a73a03c2bfa5ef5702d2cbd47950f100246f1
Olena Kostash <116300846+OlenaKostash@users.noreply.github.com>
2022-11-12 12:38:27 +0200

    Homework 5 - Методи

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -10,13 +10,17 @@ namespace TestProject
             #region 1,2,4 Paragraph
             Random random = new Random();
             List<int> sorseArray = new List<int>(); 
-            for(int i = 0; i < 4; i++)
+            for(int i = 0; i < 2; i++)
+            {   
                 sorseArray.Add(random.Next(0, 20));
+                Console.WriteLine("Used number " + sorseArray[i]);            
+            }         
 
             Console.WriteLine($"max number from {sorseArray[0]} and {sorseArray[1]}"
                 + " is " + MaxNumber(sorseArray[0], sorseArray[1]));
             Console.WriteLine($"min number from {sorseArray[0]} and {sorseArray[1]}"
                + " is " + MinNumber(sorseArray[0], sorseArray[1]));
+            Console.WriteLine();
             #endregion
 
             #region 3 Paragraph
@@ -24,10 +28,28 @@ namespace TestProject
             bool resultFromSum = TrySumIfOdd(sorseArray[0], sorseArray[1],out sum);
             Console.WriteLine($"{resultFromSum}: the sum between " +
                 $"{sorseArray[0]} and {sorseArray[1]} is {sum}");
+            Console.WriteLine();
             #endregion
 
-            Console.WriteLine("Enter a value for x");
-            bool isParseblX = Int32.TryParse(Console.ReadLine(), out x);
+            #region extra
+            Console.WriteLine("Enter any string");
+            string x = Console.ReadLine();
+
+            bool isParseblX = false;
+            int n;
+            do
+            {
+                Console.WriteLine("Enter a value for n");  
+                isParseblX = Int32.TryParse(Console.ReadLine(), out n);
+            }
+            while (!isParseblX);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(x);
+            }
+
+            #endregion
 
             Console.ReadKey();
         }
